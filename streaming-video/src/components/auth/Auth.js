fd1452001d5ade7231f4bb43df7b45b7921cdea5
Tosin17 <kajopelayeola@gmail.com>
2019-10-15 19:57:49 -0400

    Add google auth

--- streaming-video/src/components/auth/Auth.js
+++ streaming-video/src/components/auth/Auth.js
@@ -3,24 +3,73 @@ import { connect } from 'react-redux';
 
 import { signIn, signOut } from '../../actions';
 import './Auth.scss';
+import { client_id } from '../env';
 
 class Auth extends React.Component {
-  componentDidMount() {}
+  auth = null;
 
-  renderButton() {}
+  componentDidMount() {
+    window.gapi.load('auth2', () => {
+      window.gapi.auth2
+        .init({
+          client_id
+        })
+        .then(_ => {
+          this.auth = window.gapi.auth2.getAuthInstance();
 
-  render() {
-    console.log(this.props);
-    return (
-      <button className="auth__button" type="button">
+          // Cuz `this.auth.isSignedIn.listen` doesn't trigger on page init.
+          this.reRenderComponent(this.auth.isSignedIn.get());
+          this.auth.isSignedIn.listen(this.reRenderComponent);
+        });
+    });
+  }
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
+  reRenderComponent = isSignedIn => {
+    // Update `state` to re-render component to show appropriate button
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
+  };
+
+  renderButton() {
+    if (this.props.isSignedIn === null) {
+      return null;
+    }
+
+    const loginButton = (
+      <button className="auth__button" type="button" onClick={this.onSignIn}>
         Log in
       </button>
     );
+
+    const logoutButton = (
+      <button className="auth__button" type="button" onClick={this.onSignOut}>
+        Log out
+      </button>
+    );
+
+    return this.props.isSignedIn ? logoutButton : loginButton;
+  }
+
+  render() {
+    console.log(this.props);
+    return this.renderButton();
   }
 }
 
 const mapStateToProps = state => ({
-  isSignedIn: state.auth.isSignedIn
+  isSignedIn: state.auth.isSignedIn,
+  state
 });
 
 export default connect(
